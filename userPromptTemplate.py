c1399bbf35cb405a66128c06278d9786ebb4f34e
Jasper Pell <72871978+Baggins5066@users.noreply.github.com>
2025-10-02 14:15:45 -0400

    promptUser() takes input and returns it

--- userPromptTemplate.py
+++ userPromptTemplate.py
@@ -2,7 +2,17 @@ def promptUser(prompt: str, *options: str):
     print(f"\n{prompt}")
     for i, option in enumerate(options, 1):
         print(f"[{i}] {option}")
-    print("> ")
-    
+    while True:
+        try:
+            choice = int(input("> "))
+            if 1 <= choice <= len(options):
+                return options[choice - 1]
+            else:
+                print("ERROR: Invalid input.")
+        except ValueError:
+            print("ERROR: Invalid input.")
+
+# promptUser() example
 print("\nWelcome to [Game Title]")
 promptUser("Choose your class.", "Warrior", "Mage", "Rogue")
+print()
