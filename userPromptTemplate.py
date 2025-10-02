cba39cb72ca6a5beba95a7e40e2998096ebfe830
Jasper Pell <72871978+Baggins5066@users.noreply.github.com>
2025-10-02 12:48:28 -0400

    Called funtion; changed formatting

--- userPromptTemplate.py
+++ userPromptTemplate.py
@@ -1,7 +1,11 @@
-def promptUser(prompt, option1, option2, option3):
+def promptUser(prompt: str, option1: str, option2: str, option3: str):
     print(f"""
-    {prompt}
-    [1] {option1}
-    [2] {option2}
-    [3] {option3}
+{prompt}
+[1] {option1}
+[2] {option2}
+[3] {option3}
+> 
     """)
+
+print("\nWelcome to [Game Title]")
+promptUser("Choose your class.", "Warrior", "Mage", "Rogue")
