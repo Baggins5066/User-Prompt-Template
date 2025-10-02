f8e16b64d025193ced5613c6fd71f7ff039d6584
Jasper Pell <72871978+Baggins5066@users.noreply.github.com>
2025-10-02 12:56:18 -0400

    Added arrow for user input

--- userPromptTemplate.py
+++ userPromptTemplate.py
@@ -2,7 +2,7 @@ def promptUser(prompt: str, *options: str):
     print(f"\n{prompt}")
     for i, option in enumerate(options, 1):
         print(f"[{i}] {option}")
-    print()
+    print("> ")
     
 print("\nWelcome to [Game Title]")
 promptUser("Choose your class.", "Warrior", "Mage", "Rogue")
