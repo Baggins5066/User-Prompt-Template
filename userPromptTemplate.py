22d2a76f144b57b8f2dd71ac2bb62704986b3c93
Jasper Pell <72871978+Baggins5066@users.noreply.github.com>
2025-10-02 12:54:26 -0400

    Remade function to handle any number of options

--- userPromptTemplate.py
+++ userPromptTemplate.py
@@ -1,11 +1,8 @@
-def promptUser(prompt: str, option1: str, option2: str, option3: str):
-    print(f"""
-{prompt}
-[1] {option1}
-[2] {option2}
-[3] {option3}
-> 
-    """)
-
+def promptUser(prompt: str, *options: str):
+    print(f"\n{prompt}")
+    for i, option in enumerate(options, 1):
+        print(f"[{i}] {option}")
+    print()
+    
 print("\nWelcome to [Game Title]")
 promptUser("Choose your class.", "Warrior", "Mage", "Rogue")
